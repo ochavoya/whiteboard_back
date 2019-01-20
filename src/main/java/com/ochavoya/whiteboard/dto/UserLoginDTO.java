7df1b7ab32fa5cbbb0eeae4a62063a307104c270
Oscar Chavoya Aceves <chavoyao@gmail.com>
2019-01-20 16:44:05 -0700

    Updated test cases. Integration test still pending.

--- src/main/java/com/ochavoya/whiteboard/dto/UserLoginDTO.java
+++ src/main/java/com/ochavoya/whiteboard/dto/UserLoginDTO.java
@@ -12,6 +12,11 @@ public class UserLoginDTO {
     @Length(min=7)
     private String password;
 
+    public UserLoginDTO(@NotNull @Length(min = 1, max = 16) String username, @NotNull @Length(min = 7) String password) {
+        this.username = username;
+        this.password = password;
+    }
+
     public String getUsername() {
         return username;
     }
