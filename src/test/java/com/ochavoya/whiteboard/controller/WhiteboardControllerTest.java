15e31f6951df21c5a844d84e0f0ae3f5030303ad
ochavoya <chavoyao@gmail.com>
2019-01-26 12:17:59 -0700

    Updated application.properties

--- src/test/java/com/ochavoya/whiteboard/controller/WhiteboardControllerTest.java
+++ src/test/java/com/ochavoya/whiteboard/controller/WhiteboardControllerTest.java
@@ -2,6 +2,7 @@ package com.ochavoya.whiteboard.controller;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ochavoya.whiteboard.dto.UserRegisterDTO;
 import com.ochavoya.whiteboard.service.UserRepositoryService;
 import com.ochavoya.whiteboard.service.WhiteboardDataRepositoryService;
 import org.junit.Test;
@@ -10,18 +11,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(WhiteboardController.class)
 @AutoConfigureMockMvc
+@Transactional
 public class WhiteboardControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext wac;
 
     @MockBean
     private WhiteboardDataRepositoryService whiteboardDataRepositoryService;
@@ -33,6 +43,16 @@ public class WhiteboardControllerTest {
     public ObjectMapper objectMapper;
 
 
+    private UserRegisterDTO userRegisterDTO = new UserRegisterDTO();
+
+    {
+
+        userRegisterDTO.setUsername("__test__");
+        userRegisterDTO.setName("John Doe");
+        userRegisterDTO.setPassword("password");
+    }
+
+
     @Test
     public void test_mockMvcNotNull () {
         assertNotNull(mockMvc);
@@ -40,4 +60,27 @@ public class WhiteboardControllerTest {
         assertNotNull(whiteboardDataRepositoryService);
         assertNotNull(userRepositoryService);
     }
+
+    @Test
+    public void registerValidUser() throws Exception {
+
+        mockMvc.perform(post("/whiteboard/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content((objectMapper.writeValueAsString(userRegisterDTO)))
+        ).andExpect(status().isAccepted());
+    }
+
+    @Test
+    @Transactional
+    public void testLogin() throws Exception {
+
+        userRepositoryService.register(userRegisterDTO);
+        mockMvc.perform(post("/whiteboard/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"username\": \"__test__\", \"password\" : \"password\" }")
+        ).andExpect(status().isAccepted());
+
+    }
+
+
 }
